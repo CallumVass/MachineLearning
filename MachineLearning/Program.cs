7e0c7a381a6550fb0955ffda79bea5322681c016
aaroncolclough <xaaroncx@live.co.uk>
2017-01-05 10:26:35 +0000

    options loop

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -10,7 +10,8 @@ namespace MachineLearning
 
     public class Program
     {
-       public static void Main(string[] args)
+        private static MatchPredictor data;
+        public static void Main(string[] args)
         {
             var webClient = new WebClient();
             var fileData = webClient.DownloadString("http://elysiantech.co.uk:3000/Results1516header.csv");
@@ -19,25 +20,42 @@ namespace MachineLearning
                 var reader = new CsvReader(sr);
                 var records = reader.GetRecords<Match>().ToList();
                 var parser = new MatchParser(records);
-                var data = parser.Build();
-                Console.WriteLine("Please enter a home team: ");
-                var home = Console.ReadLine();
-                Console.WriteLine("Please enter an away team: ");
-                var away = Console.ReadLine();
-                //var result = data.GetScore(home,away);
-                var probs = data.ProbabilityTable(home, away);
-                var pct = data.ResultProbability(probs);
-
-                var highest = data.FindHighestAsString(probs);
-
-                Console.WriteLine(" ");
-                Console.WriteLine("------------------------");
-                Console.WriteLine(string.Format("{0}: {1}%, {2}: {3}%, {4}: {5}%", home, pct.Home, away, pct.Away, "draw", pct.Draw));
-                Console.WriteLine(string.Format("Likeliest Result: {0}", highest));
-                Console.WriteLine("------------------------");
-                Console.WriteLine(" ");
-
-                Console.ReadLine();
+                data = parser.Build();
+                Options();
             }
+        }
+
+        public static void Options()
+        {
+            Console.WriteLine("Please select an option: ");
+            Console.WriteLine("1: Score and Win Prediction");
+            Console.WriteLine("2: ");
+            Console.WriteLine("3: ");
+           var decision = Console.ReadLine();
+           if (decision == "1") ScoreAndWinPrediction();
+            Options();
+        }
+
+        public static void ScoreAndWinPrediction()
+        {
+            Console.WriteLine("Please enter a home team: ");
+            var home = Console.ReadLine();
+            Console.WriteLine("Please enter an away team: ");
+            var away = Console.ReadLine();
+            //var result = data.GetScore(home,away);
+            var probs = data.ProbabilityTable(home, away);
+            var pct = data.ResultProbability(probs);
+
+            var highest = data.FindHighestAsString(probs);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(string.Format("{0}: {1}%, {2}: {3}%, {4}: {5}%", home, pct.Home, away, pct.Away,
+                "draw", pct.Draw));
+            Console.WriteLine(string.Format("Likeliest Result: {0}", highest));
+            Console.WriteLine("------------------------");
+            Console.WriteLine(" ");
+            
+        }
     }
 }
